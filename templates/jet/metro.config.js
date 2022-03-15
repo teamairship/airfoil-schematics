0765b2a6322a2b97a7f476969c5155b22c777d5d
Amy Eddins <amy.eddins@teamairship.com>
2022-03-15 10:06:49 -0500

    Add cjs in resolver in metro config as an Apollo supported workaround for the issue where the Metro bundler config does not include the cjs extension by default

--- templates/jet/metro.config.js
+++ templates/jet/metro.config.js
@@ -5,6 +5,9 @@
  * @format
  */
 
+const { getDefaultConfig } = require('metro-config');
+const { resolver: defaultResolver } = getDefaultConfig.getDefaultValues();
+
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
@@ -13,5 +16,9 @@ module.exports = {
         inlineRequires: true,
       },
     }),
+  },
+  resolver: {
+    ...defaultResolver,
+    sourceExts: [...defaultResolver.sourceExts, 'cjs'],
   },
 };
